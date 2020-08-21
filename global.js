6fc52a0457d4907b602a3def0382795625ee48bd
balaji soundar <sbalaji1192@gmail.com>
2020-08-21 13:36:56 +0530

    More changes

--- global.js
+++ global.js
@@ -1,55 +1,14 @@
-const Jscode = "window.noCarousel = __pendoSlideList.length == 1;\n" +
-    "for (let i = 0; i < __pendoSlideList.length; i++) {\n" +
-    "\tappend(\".slider-container\", \n" +
-    "  `<div class='slider ${ i == 0 ? 'active': ''}'>\n" +
-    "  \t\t<h3>${__pendoSlideList[i].header}</h3>\n" +
-    "      ${\n" +
-    "      __pendoSlideList[i].image ? `<div>\n" +
-    "      \t<img src='${__pendoSlideList[i].image}'/>\n" +
-    "      </div>` : ''\n" +
-    "      }\n" +
-    "      <p>${__pendoSlideList[i].content}</p>\n" +
-    "   </div>`\n" +
-    "  );\n" +
-    "  if (!window.noCarousel) {\n" +
-    "  \tappend(\".slider-nav-container\", `<span class=' ${ i == 0 ? 'active': ''}' data-index='${i}'></span>`);\n" +
-    "  }\n" +
-    "}\n" +
-    "\n" +
-    "get(\".slider-nav-container\")\n" +
-    "\t.addEventListener(\"click\", (e) => {\n" +
-    "    if (e.target.dataset.index !== undefined) {\n" +
-    "      let index =\te.target.dataset.index;\n" +
-    "      \n" +
-    "      get(`.slider-nav-container span.active`).classList.remove(\"active\");\n" +
-    "      e.target.classList.add(\"active\");\n" +
-    "      \n" +
-    "      get(\".slider.active\").classList.remove(\"active\");\n" +
-    "      get(`.slider:nth-child(${Number(index) + 1})`).classList.add(\"active\");\n" +
-    "    }\n" +
-    "\n" +
-    "  });\n" +
-    "\n" +
-    "function append(ele, html) {\n" +
-    "\tele = get(ele);\n" +
-    "\tele.innerHTML = ele.innerHTML + html;\n" +
-    "}\n" +
-    "\n" +
-    "function get(ele) {\n" +
-    "\treturn document.querySelector(ele);\n" +
-    "}";
-
 $(document).ready(() => {
     $(".exporter").hide();
     $("#export").on("click", () => {
-        let slide = [],
+        let steps = [],
             promises = [];
 
-       $(".slide").each((i, ele) => {
-           if ($(ele).find(".slide-content").val()) {
+       $(".step").each((i, ele) => {
+           if ($(ele).find(".step-content").val()) {
 
                let promise = new Promise((resolve) => {
-                   let image = $(ele).find(".slide-image")[0].files;
+                   let image = $(ele).find(".step-image")[0].files;
                    if (image && image[0]) {
                        var reader = new FileReader();
 
@@ -65,58 +24,91 @@ $(document).ready(() => {
                });
 
                promise.then((image) => {
-                   slide.push({
-                       header: $(ele).find(".slide-header").val(),
+                   steps[i] = {
+                       header: $(ele).find(".step-header").val(),
                        image: image,
-                       content: $(ele).find(".slide-content").val()
-                   });
+                       content: $(ele).find(".step-content").val(),
+                       size: $(ele).find(".step-size").val()
+                   };
                });
 
                promises.push(promise);
            }
-
-           Promise.all(promises)
-               .then(() => {
-                   let js = 'window.__pendoSlideList = ' + JSON.stringify(slide) + ';\n\n';
-
-                   $(".js")
-                       .val(js + Jscode);
-
-                   $(".exporter").show();
-                   $(".form").hide();
-
-                   $('iframe')[0].src = 'data:text/html;charset=utf-8,' + escape (`
-                    <head>
-                        <style>
-                            ${$('.css').val()}
-                        </style>
-                    </head>
-                    <body>
-                        ${$('.html').val()}
-                        <script>
-                            ${$('.js').val()}
-                        </script>
-                    </body>
-               `);
-               });
        })
+
+        Promise.all(promises)
+            .then(() => {
+                steps.forEach((d, j) => {
+                    let step = $(".exporter-item:first-child")
+                        .clone()
+                        .show()
+                        .prepend(`<h1>Step ${++j}</h1>`);
+
+                    step.find(".html")
+                        .val(`
+                            <div class="step-container ${d.size}">
+                                <div class="step-body">
+                                    <div style="${d.image ? '' : 'display: none'}">
+                                        <img src="${d.image || ''}">
+                                    </div>
+                                    <div class="step-body-header">
+                                        ${d.header}
+                                    </div>
+                                    <p>
+                                        ${d.content}
+                                    </p>
+                                </div>
+                                <div class="step-footer clearfix">
+                                    <div><button onclick="skip()" class="skip-btn">Skip</button></div>
+                                    <div class="step-nav"></div>
+                                    <div>
+                                        <button onclick="next()" class="pull-right got-btn">Got it</button>
+                                        <button class="pull-right next-btn" onclick="next()">Next</button>
+                                    </div>
+                                </div>
+                            </div>
+                          `);
+
+                    $(".exporter").append(step);
+                });
+                $(".exporter").show();
+                $(".form").hide();
+            });
     });
 
     $(".reset").on("click", () => {
         window.location.reload();
     });
 
-    $("#add-slide").on("click", () => {
-        $(".slide-container").append('<div class="slide">\n' +
-            '                <div>header : <textarea class="slide-header"></textarea></div>\n' +
-            '                <div>image: <input class="slide-image" type="file" /></div>\n' +
-            '                <div>content: <textarea class="slide-content"></textarea></div>\n' +
-            '                <div><button class="slide-remove">remove</button></div>' +
-            '            </div>');
+    $("#add-step").on("click", () => {
+        let i = $(".step").length;
+
+        $(".step-container").append(`
+            <div class="step">
+                <h2> Step ${ ++i }</h2>
+                <div>header : <textarea class="step-header"></textarea></div>
+                <div>image: <input class="step-image" type="file" /></div>
+                <div>content: <textarea class="step-content"></textarea></div>
+                <div>
+                    size: <select name="size" class="step-size">
+                            <option value="big">
+                                big
+                            </option>
+                            <option value="small">
+                                small
+                            </option>
+                            <option value="small-no-image">
+                                small with no image
+                            </option>
+                        </select>
+                </div>
+                <div><button class="step-remove">remove</button></div>
+            </div>
+        `);
     });
 
-    $(".slide-container").on("click", (e) => {
-       if ($(e.target).hasClass('slide-remove')) {
+    $(".step-container").on("click", (e) => {
+       if ($(e.target).hasClass('step-remove')) {
            $(e.target).parent().parent().remove();
        }
     });
